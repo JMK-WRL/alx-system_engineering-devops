5d3fa599e16b0af0eb2d1cda8028126343905ee8
Jonathan <jonathankyule2@gmail.com>
2023-11-25 11:15:22 -0800

    task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Puppet manifest to create a file in /tmp
+
+file { '/tmp/school':
+  ensure  => 'file',            # Ensure it's a regular file
+  mode    => '0744',            # Set file permissions
+  owner   => 'www-data',        # Set file owner
+  group   => 'www-data',        # Set file group
+  content => 'I love Puppet',   # Set the content of the file
+}
