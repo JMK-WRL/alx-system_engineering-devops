b979532149f66575700d0197e26fbc696a1729c8
Jonathan <jonathankyule2@gmail.com>
2024-01-16 13:04:18 -0800

    correction

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,18 @@
 #!/usr/bin/python3
 """
-number of subscribers for a given subreddit
+A fuction that queries the Reddit API and returns the
+number of subscribers (not active users, total subscribers)
+for a given subreddit. If an invalid subreddit is given,
+the function should return 0.
 """
-
-from requests import get
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    function that queries the Reddit API and returns the number of subscribers
-    (not active users, total subscribers) for a given subreddit.
-    """
-
-    if subreddit is None or not isinstance(subreddit, str):
-        return 0
-
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent)
-    results = response.json()
-
-    try:
-        return results.get('data').get('subscribers')
-
-    except Exception:
+    about_url = 'https://www.reddit.com/r/' + subreddit + '/about.json'
+    response = requests.get(about_url, headers={'User-Agent': 'YourApp/1.0'})
+    if response.status_code == 200:
+        data = response.json()
+        return int(data['data']['subscribers'])
+    else:
         return 0
