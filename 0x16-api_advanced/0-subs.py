22d83c75dcbd6ead22a2bd3710d20aa0d4ce0831
Jonathan <jonathankyule2@gmail.com>
2024-01-18 01:00:54 -0800

    pep8

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,6 +14,7 @@ def number_of_subscribers(subreddit):
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
+        
         if response.status_code == 200:
             data = response.json().get("data", {})
             subscribers_count = data.get("subscribers", 0)
