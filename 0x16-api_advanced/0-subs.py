226b0ef403b7eb7243c50679be5eec223755d685
Jonathan <jonathankyule2@gmail.com>
2024-01-18 00:29:12 -0800

    corrections

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,13 +6,22 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
-        return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+    headers = {'User-Agent':'CustomUserAgent'}
+    url = f'https://www.redit.com/r/{subreddit}/about.json'
+
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            subreddit_info = response.json().get('data', {})
+            subscribers_count = subreddit_info.get('subscribers', 0)
+            return f"OK {subscribers_count}"
+
+        elif response.status_code == 404:
+            return "Error"
+        else: 
+            print(f"Error: {response.status_code} - {response.text}")
+            return "Error"
+
+    except requests.RequestException as e:
+        print(f"Request Exception: {e}")
+        return "Error"
